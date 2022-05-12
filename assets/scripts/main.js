32c6401b4e26eecef4ca05fd65349b9bbb44f776
Valentyn Troian <troval12989@gmail.com>
2022-05-12 18:15:24 +0300

    Show background

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,12 +2,14 @@ let scene = new Phaser.Scene("Game");
 
 scene.preload = function() {
     // 1. load background
-    console.log("preload");
+    this.load.image('bg', 'assets/sprites/background.jpg');
 };
 
 scene.create = function() {
-    // 2. show background
-    console.log("create");
+    // 2. show background  
+    // let width = this.sys.game.config.width / 2,
+    //     height = this.sys.game.config.height / 2;
+    this.add.sprite(0, 0, 'bg').setOrigin(0, 0);
 };
 
 let config = {
